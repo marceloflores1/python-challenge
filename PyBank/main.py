95c3a533cc768d1535b737b3684f4e595bbb8b8b
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-04-04 20:27:57 -0500

    After LOOOONG hours finally got PyPoll script to work, deleted original file replaced with different solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,27 +5,23 @@ budgetdata_csv = os.path.join('Resources','budget_data.csv')
 
 with open(budgetdata_csv,'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    months = -1
+    months = 0
     net_profitloss = 0
     greatest_increase = 0
     greatest_decrease = 0
     average_change = 0
     for row in csvreader:
-        if months == -1:
-            print('Financial Analysis')
-            print('----------------------------')
-            months += 1
-        else:
-  #          print(f'\t{row[0]} date and {row[1]} profit/loss')
-            months += 1
-            net_profitloss += int(row[1])
-            if int(row[1]) > int(greatest_increase):
-                greatest_increase = int(row[1])
-                greatest_increase_month = row[0]
-            if int(row[1]) < int(greatest_decrease):
-                greatest_decrease = int(row[1])
-                greatest_decrease_month = row[0]
+        months += 1
+        net_profitloss += int(row[1])
+        if int(row[1]) > int(greatest_increase):
+            greatest_increase = int(row[1])
+            greatest_increase_month = row[0]
+        if int(row[1]) < int(greatest_decrease):
+            greatest_decrease = int(row[1])
+            greatest_decrease_month = row[0]
     average_change = net_profitloss/months        
+    print('Financial Analysis')
+    print('----------------------------')
     print(f'Total Months: {months}')
     print(f'Total: ${net_profitloss}')
     print(f'Average Change: ${"{:.2f}".format(average_change)}')
---
+++ PyPoll/main.py
@@ -0,0 +1,34 @@
+import os
+import csv
+
+electiondata_csv = os.path.join('Resources','election_data.csv')
+
+with open(electiondata_csv,'r') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=',')
+    header = next(csvreader)
+    election_dict = {}
+    candidate_dict = {}
+    for row in csvreader:
+        election_dict[row[0]] = (row[2])
+        candidate_dict[row[2]] = 0
+    for candidate in election_dict.values():
+        candidate_dict[(candidate)] += 1
+
+print('Election Results')
+print('----------------------------')
+print(f'Total votes: {len(election_dict)}')
+print('----------------------------')
+for k,v in candidate_dict.items():
+    print(f'{k}: {"{:.3f}".format(v/len(election_dict)*100)}% ({v})')
+print('----------------------------')
+print(f'Winner: {max(candidate_dict, key=candidate_dict.get)}')
+
+f = open("Analysis/Election Results.txt", "w")
+f.write("Election Results")
+f.write("\n----------------------------")    
+f.write(f'\nTotal Votes: {len(election_dict)}')
+f.write("\n----------------------------")    
+for k,v in candidate_dict.items():
+    f.write(f'\n{k}: {"{:.3f}".format(v/len(election_dict)*100)}% ({v})')
+f.write("\n----------------------------")    
+f.write(f'\nWinner: {max(candidate_dict, key=candidate_dict.get)}')    
